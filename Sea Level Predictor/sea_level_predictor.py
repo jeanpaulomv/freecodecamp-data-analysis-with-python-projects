e68cf8a8ee2d63b786f730e158b7896ff9678496
Jean Paulo Moran Vanoni <jeanpaulomv@homtail.com>
2024-05-29 01:42:47 -0500

    Initialize data analysis projects

---
+++ Sea Level Predictor/sea_level_predictor.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from scipy.stats import linregress
+
+def draw_plot():
+    # Read data from file
+    df = pd.read_csv('epa-sea-level.csv')
+
+    # Create scatter plot
+    plt.figure(figsize=(10, 6))
+    plt.scatter(df['Year'], df['CSIRO Adjusted Sea Level'], c='blue', label='Measured Sea Level')
+
+    # Create first line of best fit
+    slope, intercept, _, _, _ = linregress(df['Year'], df['CSIRO Adjusted Sea Level'])
+    x_pred = pd.Series([i for i in range(1880, 2051)])
+    y_pred = slope * x_pred + intercept
+    plt.plot(x_pred, y_pred, 'r', label='Best Fit Line 1')
+
+    # Create second line of best fit
+    df_recent = df[df['Year'] >= 2000]
+    slope_recent, intercept_recent, _, _, _ = linregress(df_recent['Year'], df_recent['CSIRO Adjusted Sea Level'])
+    x_pred_recent = pd.Series([i for i in range(2000, 2051)])
+    y_pred_recent = slope_recent * x_pred_recent + intercept_recent
+    plt.plot(x_pred_recent, y_pred_recent, 'g', label='Best Fit Line 2')
+
+    # Add labels and title
+    plt.xlabel('Year')
+    plt.ylabel('Sea Level (inches)')
+    plt.title('Rise in Sea Level')
+    plt.legend()
+    
+    # Save plot and return data for testing (DO NOT MODIFY)
+    plt.savefig('sea_level_plot.png')
+    return plt.gca()
---
+++ readme.md
@@ -0,0 +1,1 @@
+![Certificate](Certificate in Data Analysis with Python Awarded to Jean Paulo.png)
